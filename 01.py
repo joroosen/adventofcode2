8d45e4449b3a419a86f660cecf3104c7cc32ca3a
joroosen <Fak3l0v3>
2020-12-26 00:05:40 +0100

    Day 1 - Part 2

--- 01.py
+++ 01.py
@@ -6,9 +6,9 @@ with open("01_input.txt") as f:
     mylist = f.read().splitlines()
 
 
-for a, b in itertools.combinations(mylist, 2):
-    if int(a) + int(b) == 2020:
-        print(int(a) * int(b))
+for a, b, c in itertools.combinations(mylist, 3):
+    if int(a) + int(b) + int(c) == 2020:
+        print(int(a) * int(b) * int(c))
         break
 
 print("--- %s seconds ---" % (time.time() - start_time))
